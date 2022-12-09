baa5eecd4bfb63ccf356fa1925f8f75c435a4033
Alexander Lukyanov <lux-nova@mail.ru>
2022-12-09 15:53:27 +0300

    Field standardized, app init

--- src/Field.java
+++ src/Field.java
@@ -1,38 +1,49 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Field {
     char[][] cells;
-    int ownerX = -1, ownerY = -1;
+    int masterX = -1, masterY = -1;
 
     public Field(char[][] cells) {
         this.cells = cells;
     }
 
-    public Field(char[][] cells, int ownerX, int ownerY) {
+    public Field(char[][] cells, int masterX, int masterY) {
         this.cells = cells;
-        this.ownerX = ownerX;
-        this.ownerY = ownerY;
+        this.masterX = masterX;
+        this.masterY = masterY;
     }
 
     public static Field parse(String field) {
         int x = -1;
         int y = -1;
-        List<char[]> rows = new ArrayList<>();
         String[] lines = field.split("\n");
+        Arrays.setAll(lines,
+                i -> lines[i].replaceAll("\\s+", ""));
 
         if (lines.length < 1 || lines[0].isBlank())
             throw new IllegalArgumentException("Поле не найдено!");
+
+
         if (!lines[0].startsWith("Щ"))
             throw new IllegalArgumentException("В начале этого поля нет щенка!");
 
+        List<char[]> rows = new ArrayList<>(lines.length);
+        int width = lines[0].length();
+
         for (int i = 0; i < lines.length; i++) {
             String s = lines[i];
+
+            if (s.length() != width)
+                throw new IllegalArgumentException("Поле не прямоугольное!");
+
             if (s.indexOf('Ч') > -1) {
                 if (x != -1)
                     throw new IllegalArgumentException("На этом поле более одного хозяина!");
-                x = i;
-                y = s.indexOf('Ч');
+                x = s.indexOf('Ч');
+                y = i;
             }
             rows.add(s.toCharArray());
         }
@@ -47,18 +58,18 @@ public class Field {
         System.out.println("Кактусовое поле, на котором щенок ищет хозяина:");
         StringBuilder sb = new StringBuilder();
         for (char[] line : cells) {
-            for (char c : line) sb.append(c);
+            for (char c : line) sb.append(c).append(" ");
             sb.append('\n');
         }
         System.out.print(sb);
-        System.out.printf("Хозяин находится в координатах %s:%s", ownerX, ownerY);
+        System.out.printf("Хозяин находится в координатах %s:%s%n", masterX, masterY);
     }
 
     public boolean isReachableFromLeft(int x, int y) {
-        return x > 0 && cells[x - 1][y] != '*';
+        return x > 0 && cells[y][x - 1] != '*';
     }
     public boolean isReachableFromAbove(int x,  int y) {
-        return y > 0 && cells[x][y - 1] != '*';
+        return y > 0 && cells[y - 1][x] != '*';
     }
 
     public char checkCellReachability(int x, int y) {
@@ -74,13 +85,13 @@ public class Field {
         for (int i = 0; i < cells.length; i++) {
             for (int j = 0; j < cells[i].length; j++)
                 sb.append(
-                    cells[i][j] == '-' ?
-                        checkCellReachability(i, j) :
+                    cells[i][j] == '-' || cells[i][j] == 'Ч' ?
+                        checkCellReachability(j, i) :
                             cells[i][j]
-                );
+                ).append(" ");
             sb.append("\n");
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Field field = Field.parse("""
+                Щ - - * * - - - - -
+                - - - - * - * * - -
+                - - - * - * - - - *
+                - * - - - - - - Ч -
+                - - - - - - * - - -
+                - - * - - * - - - -
+                - - - * - - * * * -
+                - - - - - - - * - -
+                - - - - - - - * - -
+                - - - - - * * - - -""");
+
+        field.print();
+        field.printAccessibilityMask();
+
+
     }
+
+
 }
